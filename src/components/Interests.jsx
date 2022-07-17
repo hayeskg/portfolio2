434c18b7818fa1f1b3cf2b29acbcd5eec6a8ee1d
Kristof Hayes <kristof@enano.io>
2022-07-17 20:23:16 +0100

    Interests

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -12,36 +12,36 @@ const Interests = () => {
 
     <section class="interests">
       <div>
-        <h1 class="header1">IoT and Automation</h1>
+        <h1 class="header1">Appropriate Tech</h1>
         <figure>
           <img src={machine} alt="a control system layout" />
         </figure>
         <p>
-          Connecting things. Building data landscapes. Improving processes.
+          Connecting things. Making them move. Benchmark and improve processes.
             </p>
         <figure>
           <img src={iiot3} alt="industrial robot" />
         </figure>
       </div>
       <div>
-        <h1 class="header1">Software Development</h1>
+        <h1 class="header1">System Development</h1>
         <figure>
           <img src={cs3} alt="a graphic depicting Iot" />
         </figure>
         <p>
-          Using open source tools and technologies to build rapid prototypes. Start small then scale fast to demonstrate value.
+          Using open-source resources to build appropriate tech solutions. Start small, scale at pace.
         </p>
         <figure>
           <img src={nc2} alt="northcoders logo" />
         </figure>
       </div>
       <div>
-        <h1 class="header1">Sustainability</h1>
+        <h1 class="header1">Projects</h1>
         <figure>
           <img src={eco4} alt="sustainable graphic" />
         </figure>
         <p>
-          Solving problems where technology and sustainability overlap. Building impactful businesses.
+          Develop partnerships, products and services.
         </p>
         <figure>
           <img src={loop2} alt="feedback loop" />
